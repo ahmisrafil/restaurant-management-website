4d5c72b893ecd02ed38f6d551080d7c1813db188
ahmisrafil <ahmisrafil@yahoo.com>
2023-11-23 04:16:12 +0600

    sweeper added in homepage

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Banner from "../../Shared/Banner/Banner";
+import Swipper from "../Swipper/Swipper";
 
 
 const Home = () => {
     return (
         <div>
             <Banner />
+            <Swipper></Swipper>
         </div>
     );
 };
---
+++ src/pages/Home/Swipper/Swipper.jsx
@@ -0,0 +1,45 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+// import required modules
+import { Pagination } from 'swiper/modules';
+
+import slide1 from '../../../assets/home/slide1.jpg';
+import slide2 from '../../../assets/home/slide2.jpg';
+import slide3 from '../../../assets/home/slide3.jpg';
+import slide4 from '../../../assets/home/slide4.jpg';
+import slide5 from '../../../assets/home/slide5.jpg';
+
+
+
+const Swipper = () => {
+    return (
+        <Swiper  slidesPerView={3}
+        spaceBetween={30}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper">
+        <SwiperSlide>
+            <img src={slide1} />
+        </SwiperSlide>
+        <SwiperSlide>
+            <img src={slide2} />
+        </SwiperSlide>
+        <SwiperSlide>
+            <img src={slide3} />
+        </SwiperSlide>
+        <SwiperSlide>
+            <img src={slide4} />
+        </SwiperSlide>
+        <SwiperSlide>
+            <img src={slide5} />
+        </SwiperSlide>
+        
+      </Swiper>
+    );
+};
+
+export default Swipper;
